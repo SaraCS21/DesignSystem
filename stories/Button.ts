8f884805f99a2fce93e305ab151078a6f1601098
SaraCS21 <saracs15.scssn@gmail.com>
2024-04-12 19:01:02 +0100

    feat: DS - Install lit and create first lit component

--- stories/Button.ts
+++ stories/Button.ts
@@ -1,55 +1,13 @@
-import { html } from 'lit';
-import { styleMap } from 'lit/directives/style-map.js';
-import './button.css';
+import { LitElement, html, css } from 'lit';
 
-export interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
-}
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-  primary,
-  backgroundColor,
-  size,
-  label,
-  onClick,
-}: ButtonProps) => {
-  const mode = primary
-    ? 'storybook-button--primary'
-    : 'storybook-button--secondary';
-
-  return html`
-    <button
-      type="button"
-      class=${[
-        'storybook-button',
-        `storybook-button--${size || 'medium'}`,
-        mode,
-      ].join(' ')}
-      style=${styleMap({ backgroundColor })}
-      @click=${onClick}
-    >
-      ${label}
-    </button>
+class MiBoton extends LitElement {
+  static styles = css`
+    /* estilos del botón */
   `;
-};
+
+  render() {
+    return html`<button><slot></slot></button>`;
+  }
+}
+
+customElements.define('mi-boton', MiBoton);
